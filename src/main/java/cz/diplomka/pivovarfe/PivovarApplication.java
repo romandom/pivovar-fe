54eee589d04b4387703a911f890a4b3d16071b86
romandom <dominik.roman@bluelemons.sk>
2024-12-25 19:25:43 +0100

    softverova klavesnica

--- src/main/java/cz/diplomka/pivovarfe/PivovarApplication.java
+++ src/main/java/cz/diplomka/pivovarfe/PivovarApplication.java
@@ -3,6 +3,7 @@ package cz.diplomka.pivovarfe;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,8 +22,41 @@ public class PivovarApplication extends Application {
     public static void switchScene(String fxmlPath) throws IOException {
         var loader = new FXMLLoader(PivovarApplication.class.getResource(fxmlPath));
         var scene = new Scene(loader.load(), 800, 480);
+
+        // Pridanie softvérovej klávesnice
+        attachSoftKeyboardListeners(scene);
+
         primaryStage.setScene(scene);
     }
+
+    public static void attachSoftKeyboardListeners(Scene scene) {
+        scene.getRoot().lookupAll(".text-field").forEach(node -> {
+            if (node instanceof TextField) {
+                node.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+                    try {
+                        if (isNowFocused) {
+                            // Spustenie softvérovej klávesnice
+                            try {
+                                new ProcessBuilder("matchbox-keyboard").start();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        } else {
+                            // Zatvorenie softvérovej klávesnice
+                            try {
+                                new ProcessBuilder("pkill", "matchbox-keyboard").start();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    } catch (RuntimeException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+        });
+    }
+
 
     public static void main(String[] args) {
         launch();
